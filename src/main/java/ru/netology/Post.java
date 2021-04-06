dd0b9d716fb6ae03b2154b946e48c9298cc2ff58
jukkty <dima.iachimov@gmail.com>
2021-04-06 15:32:17 +0300

    v1

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class Post {
+    private String text;
+    private int id;
+    private int numberOfLikes;
+    private int numberOfViews;
+    private int numberOfReposts;
+    private int numberOfComments;
+    private int date;
+    private String imgUrl;
+    private String authorID;
+
+}
