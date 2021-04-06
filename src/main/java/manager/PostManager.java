39f34af801786db98a1b9b86acdfaf85c45e1d8a
jukkty <dima.iachimov@gmail.com>
2021-04-06 17:37:13 +0300

    PostManager added

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,2 +1,13 @@
-package manager;public class PostManager {
+package manager;
+
+import ru.netology.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] searchPost(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return posts;
+    }
+    public void deletePost(int ownerId, int postId) {
+    }
 }
