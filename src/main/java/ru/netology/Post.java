cecf071f0e03f72f293833023f066daf50883ee3
jukkty <dima.iachimov@gmail.com>
2021-04-07 13:22:26 +0300

    v2 updated

--- src/main/java/ru/netology/Post.java
+++ src/main/java/ru/netology/Post.java
@@ -1,17 +1,15 @@
 package ru.netology;
 
 public class Post {
-    private String text;
-    private int id;
-    private int numberOfLikes;
-    private int numberOfViews;
-    private int numberOfReposts;
-    private int numberOfComments;
-    private int date;
-    private String imgUrl;
-    private String authorID;
-    private int signerId;
-    private boolean markedAsAds;
-    private int ownerId;
-
+    private LikesInfo likesInfo;        // Дополнительный класс отвечающий за лайки
+    private RepostsInfo repostsInfo;    // Дополнительный класс отвечающий за репосты
+    private CommentsInfo commentsInfo;  // Дополнительный класс отвечающий за комментарии
+    private int id;                     // Идентификационный номер поста
+    private String text;                // Текст поста
+    private int date;                   // Дата публикации
+    private int numberOfViews;          // Количество просмотров
+    private String imgUrl;              // Расположение картинки
+    private int signerId;               // Идентификатор автора,если публикация была от имени сообщества и подписана пользователем
+    private boolean markedAsAds;        // Присутствует ли реклама
+    private int ownerId;                // Идентификатор владельца стены
 }
